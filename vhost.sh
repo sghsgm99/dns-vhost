ba0d140390807cabec91ec3fbc7233f12fa9bb6c
sghsgm99 <sghsgm99@gmail.com>
2022-05-11 08:20:18 +0900

    test

--- vhost.sh
+++ vhost.sh
@@ -1,44 +1,15 @@
 #!/bin/bash
-# This script was originally used for create virtual hosts on CentOs.
-# Created by alexnogard from http://alexnogard.com
-# Improved by mattmezza from http://you.canmakethat.com
-# Adapted for Apache2 on Ubuntu by Stephen Downes at https://www.downes.ca
-# Feel free to modify it
-#   PARAMETERS
-#
-# $usr          - User
-# $dir          - directory of web files
-# $servn        - webserver address without www.
-# $cname        - cname of webserver
-# EXAMPLE
-# Web directory = /var/www/
-# ServerName    = domain.com
-# cname            = devel
-#
-#
-# Check if you execute the script as root user
-#
-# This will check if directory already exist then create it with path : /directory/you/choose/domain.com
-# Set the ownership, permissions and create a test index.php file
-# Create a vhost file domain in your /etc/httpd/conf.d/ directory.
-# And add the new vhost to the hosts.
-#
-#
+
 servn="basicbernadette.xyz"
-cname="dev4"
+cname="dev6"
 dir="/var/www/"
-usr="ubuntu"
+usr=""
 listen="*"
 
 if [ "$(whoami)" != 'root' ]; then
 echo "You have to execute this script as root user"
 exit 1;
 fi
-#read -p "Enter the server name your want (without www) : " $servn
-#read -p "Enter a CNAME (e.g. :www or dev for dev.website.com) : " $cname
-#read -p "Enter the path of directory you wanna use (e.g. : /var/www/, dont forget the /): " $dir
-#read -p "Enter the user you wanna use (e.g. : apache) : " $usr
-#read -p "Enter the listened IP for the server (e.g. : *): " $listen
 
 if [ -d "$dir$cname" ]; then
     echo "$dir$cname already exists! Try a different CNAME"
@@ -79,43 +50,6 @@ echo "Virtual host /etc/apache2/sites-enabled/$cname.$servn.conf created !"
 else
 echo "Virtual host /etc/apache2/sites-enabled/$cname.$servn.conf wasn't created !"
 fi
-echo "Would you like me to create ssl virtual host [y/n]? "
-#read q
-#if [[ "${q}" == "yes" ]] || [[ "${q}" == "y" ]]; then
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/$cname.$servn.key -out /etc/ssl/certs/$cname.$servn.crt
-if [ -e "/etc/ssl/certs/$cname.$servn.key" ]; then
-echo "Certificate key created !"
-else
-echo "Certificate key wasn't created !"
-fi
-if [ -e "/etc/ssl/certs/$cname.$servn.crt" ]; then
-echo "Certificate created !"
-else
-echo "Certificate wasn't created !"
-fi
-
-echo "#### ssl $cname $servn
-<VirtualHost $listen:443>
-SSLEngine on
-SSLCertificateFile /etc/ssl/certs/$cname.$servn.crt
-SSLCertificateKeyFile /etc/ssl/certs/$cname.$servn.key
-ServerName $alias
-ServerAlias $alias
-DocumentRoot $dir$cname/html/
-<Directory $dir$cname>
-Options Indexes FollowSymLinks MultiViews
-AllowOverride All
-Order allow,deny
-Allow from all
-Satisfy Any
-</Directory>
-</VirtualHost>" > /etc/apache2/sites-enabled/ssl.$cname.$servn.conf
-if ! echo -e /etc/apache2/sites-enabled/ssl.$cname.$servn.conf; then
-echo "SSL Virtual host wasn't created !"
-else
-echo "SSL Virtual host created !"
-fi
-#fi
 
 echo "127.0.0.1 $servn" >> /etc/hosts
 if [ "$alias" != "$servn" ]; then
@@ -135,6 +69,3 @@ echo "All work done! You should be able to see your website at http://$servn"
 echo ""
 echo "Share the love! <3"
 echo "======================================"
-echo ""
-echo "Original script? https://gist.github.com/mattmezza/2e326ba2f1352a4b42b8"
-echo "Updated for ubuntu / Apache2 https://gist.github.com/Downes/adc7e0baa21eb18bcfe1415dc5f6b697"
